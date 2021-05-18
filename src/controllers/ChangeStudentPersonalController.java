037b2f2a4ea8c4c5101a8f8f1c8157e6fcd1dbb5
Alexandr <62112154+SashaSunsh1ne@users.noreply.github.com>
2021-05-19 02:39:39 +0300

    unused statement

--- src/controllers/ChangeStudentPersonalController.java
+++ src/controllers/ChangeStudentPersonalController.java
@@ -4,7 +4,6 @@ import entities.StudentEntity;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
